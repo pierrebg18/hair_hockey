4e24d011c34793dabbe21b44f4742dc6358cc86d
Nikita Rodyhin <rodygin.nikita2005@gmail.com>
2026-03-31 20:26:44 +0200

    feat: pom.xml modification + first folder for 3d objects

---
+++ src/main/java/fr/univtln/pierre/samples/object3d/Table.java
@@ -0,0 +1,11 @@
+package fr.univtln.pierre.samples.object3d;
+
+import com.jme3.math.ColorRGBA;
+
+public class Table {
+    private float width;
+    private float lenght;
+    private float thickness;
+    private ColorRGBA color;
+
+}
