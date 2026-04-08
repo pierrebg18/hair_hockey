53bfeb7e1ea6b18d9088198e8c9132a255a1798f
yassine3634 <yassine-boulidat@etud.univ-tln.fr>
2026-04-08 10:22:08 +0100

    feat IHM

--- src/main/java/fr/univtln/pierre/samples/App.java
+++ src/main/java/fr/univtln/pierre/samples/App.java
@@ -1,9 +1,7 @@
 package fr.univtln.pierre.samples;
 
-import com.bulletphysics.dynamics.RigidBody;
 import com.jme3.app.SimpleApplication;
 import com.jme3.bullet.BulletAppState;
-import com.jme3.bullet.control.RigidBodyControl;
 import com.jme3.material.Material;
 import com.jme3.math.ColorRGBA;
 import com.jme3.math.FastMath;
@@ -13,16 +11,31 @@ import com.jme3.scene.Geometry;
 import com.jme3.scene.Node;
 import com.jme3.system.AppSettings;
 
+import com.jme3.input.KeyInput;
+import com.jme3.input.controls.ActionListener;
+import com.jme3.input.controls.KeyTrigger;
+
 import fr.univtln.pierre.samples.modele.Paddle;
 import fr.univtln.pierre.samples.modele.Puck;
 import fr.univtln.pierre.samples.modele.Side;
 import fr.univtln.pierre.samples.modele.Table;
 
-public class App extends SimpleApplication {
+public class App extends SimpleApplication implements ActionListener {
 
     private BulletAppState bulletAppState;
 
-    public static void main(String[] args){
+    private fr.univtln.pierre.samples.ui.MainMenuUI mainMenuUI;
+    private fr.univtln.pierre.samples.ui.GameHudUI gameHudUI;
+    private int selectedMenuIndex = 0;
+    private String player1Name = "Joueur 1";
+    private String player2Name = "Joueur 2";
+
+    private int score1 = 0;
+    private int score2 = 0;
+
+    private boolean menuVisible = true;
+
+    public static void main(String[] args) {
         App app = new App();
         AppSettings settings = new AppSettings(true);
         settings.setResolution(1280, 720);
@@ -31,49 +44,45 @@ public class App extends SimpleApplication {
         app.start();
     }
 
-    public App(){
+    public App() {
     }
 
     @Override
     public void simpleInitApp() {
         placeCameraUp();
+
         Node pivot = new Node("pivot");
-        rootNode.attachChild(pivot); // put this node in the scene
+        rootNode.attachChild(pivot);
 
         bulletAppState = new BulletAppState();
         stateManager.attach(bulletAppState);
 
-
-
         // table
-        Table table =  new Table(2F, 4F, 0.1F, ColorRGBA.Blue);
-        Material matTable = new Material(assetManager,
-                "Common/MatDefs/Misc/Unshaded.j3md");  // create a simple material
+        Table table = new Table(2F, 4F, 0.1F, ColorRGBA.Blue);
+        Material matTable = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
         matTable.setColor("Color", table.getColor());
         Geometry tableGeometry = table.createGeometry();
-        table.createPhysic(tableGeometry,bulletAppState);
+        table.createPhysic(tableGeometry, bulletAppState);
         tableGeometry.setMaterial(matTable);
 
         // left side of the table
         Side leftSide = new Side(table, ColorRGBA.Brown);
-        Material matSide = new Material(assetManager,
-                "Common/MatDefs/Misc/Unshaded.j3md");  // create a simple material
+        Material matSide = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
         matSide.setColor("Color", leftSide.getColor());
         Geometry leftSideGeometry = leftSide.createGeometryLeft();
-        table.createPhysic(leftSideGeometry,bulletAppState);
+        table.createPhysic(leftSideGeometry, bulletAppState);
         leftSideGeometry.setMaterial(matSide);
 
         // right side of the table
         Side rightSide = new Side(table, ColorRGBA.Brown);
         Geometry rightSideGeometry = rightSide.createGeometryRight();
-        table.createPhysic(rightSideGeometry,bulletAppState);
+        table.createPhysic(rightSideGeometry, bulletAppState);
         rightSideGeometry.setMaterial(matSide);
 
         // puck
         Puck puck = new Puck(20, 10, 0.4F, 0.2F, ColorRGBA.LightGray, table);
         puck.putOnMySide();
-        Material matPuck = new Material(assetManager,
-                "Common/MatDefs/Misc/Unshaded.j3md");  // create a simple material
+        Material matPuck = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
         matPuck.setColor("Color", puck.getColor());
         Geometry puckGeometry = puck.createGeometry();
         puck.createPhysic(puckGeometry, bulletAppState);
@@ -81,18 +90,17 @@ public class App extends SimpleApplication {
 
         // my paddle
         Paddle myPaddle = new Paddle(0.4F, 0.2F, 0.1F, ColorRGBA.Gray);
-        Material matPaddle = new Material(assetManager,
-                "Common/MatDefs/Misc/Unshaded.j3md");  // create a simple material
+        Material matPaddle = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
         matPaddle.setColor("Color", myPaddle.getColor());
         Geometry paddleGeometry = myPaddle.createGeometryMy(table);
-        myPaddle.createPhysic(paddleGeometry,bulletAppState);
+        myPaddle.createPhysic(paddleGeometry, bulletAppState);
         paddleGeometry.setMaterial(matPaddle);
 
         // opponent's paddle
         Paddle opponentPaddle = new Paddle(0.4F, 0.2F, 0.1F, ColorRGBA.Gray);
         Geometry opponentPaddleGeometry = opponentPaddle.createGeometryOpponent(table);
         opponentPaddleGeometry.setMaterial(matPaddle);
-        opponentPaddle.createPhysic(opponentPaddleGeometry,bulletAppState);
+        opponentPaddle.createPhysic(opponentPaddleGeometry, bulletAppState);
 
         pivot.attachChild(tableGeometry);
         pivot.attachChild(leftSideGeometry);
@@ -100,25 +108,181 @@ public class App extends SimpleApplication {
         pivot.attachChild(puckGeometry);
         pivot.attachChild(paddleGeometry);
         pivot.attachChild(opponentPaddleGeometry);
+
+        // UI
+        mainMenuUI = new fr.univtln.pierre.samples.ui.MainMenuUI(this);
+        gameHudUI = new fr.univtln.pierre.samples.ui.GameHudUI(this);
+
+        initKeys();
+        showMenu();
     }
 
-    public void placeCameraPlayerSide(){
+    private void initKeys() {
+        inputManager.addMapping("MENU_UP", new KeyTrigger(KeyInput.KEY_UP));
+        inputManager.addMapping("MENU_DOWN", new KeyTrigger(KeyInput.KEY_DOWN));
+        inputManager.addMapping("SELECT", new KeyTrigger(KeyInput.KEY_RETURN));
+        inputManager.addMapping("CHANGE_PLAYERS", new KeyTrigger(KeyInput.KEY_U));
+        inputManager.addMapping("ADD_SCORE_LEFT", new KeyTrigger(KeyInput.KEY_A));
+        inputManager.addMapping("ADD_SCORE_RIGHT", new KeyTrigger(KeyInput.KEY_P));
+        inputManager.addMapping("BACK_OR_QUIT", new KeyTrigger(KeyInput.KEY_ESCAPE));
+
+        inputManager.addListener(this,
+                "MENU_UP",
+                "MENU_DOWN",
+                "SELECT",
+                "CHANGE_PLAYERS",
+                "ADD_SCORE_LEFT",
+                "ADD_SCORE_RIGHT",
+                "BACK_OR_QUIT");
+    }
+
+    private void executeSelectedMenu() {
+        switch (selectedMenuIndex) {
+            case 0:
+                startGame();
+                break;
+            case 1:
+                changePlayers();
+                break;
+            case 2:
+                stop();
+                break;
+            default:
+                break;
+        }
+    }
+
+    public void placeCameraPlayerSide() {
         cam.setLocation(new Vector3f(0, 4f, 9f));
         cam.lookAt(new Vector3f(0, 0, 0), new Vector3f(0, 1, 1));
     }
 
-    public void placeCameraOpponentSide(){
+    public void placeCameraOpponentSide() {
         cam.setLocation(new Vector3f(0, 4f, -9f));
         cam.lookAt(new Vector3f(0, 0, 0), new Vector3f(0, 1, 1));
     }
 
-    // rotation to adjust by 180-degree rotation around table
-    public void placeCameraUp(){
+    public void placeCameraUp() {
         cam.setLocation(new Vector3f(0, 12f, 0f));
         cam.lookAt(new Vector3f(0, 0, 0), new Vector3f(0, -1, 0));
         Quaternion roll90x = new Quaternion();
-        roll90x.fromAngleAxis(FastMath.PI/2, new Vector3f(1,0,0));
+        roll90x.fromAngleAxis(FastMath.PI / 2, new Vector3f(1, 0, 0));
         cam.setRotation(roll90x);
     }
 
+    public void showMenu() {
+        guiNode.detachAllChildren();
+        menuVisible = true;
+        mainMenuUI.show(player1Name, player2Name, selectedMenuIndex);
+    }
+
+    public void showHud() {
+        guiNode.detachAllChildren();
+        menuVisible = false;
+        gameHudUI.show(player1Name, player2Name, score1, score2);
+    }
+
+    public void startGame() {
+        score1 = 0;
+        score2 = 0;
+        showHud();
+    }
+
+    public void changePlayers() {
+        if (player1Name.equals("Joueur 1")) {
+            player1Name = "Yassine";
+            player2Name = "Invité";
+        } else {
+            player1Name = "Joueur 1";
+            player2Name = "Joueur 2";
+        }
+
+        if (menuVisible) {
+            showMenu();
+        } else {
+            showHud();
+        }
+    }
+
+    public void addScoreLeft() {
+        score1++;
+        showHud();
+    }
+
+    public void addScoreRight() {
+        score2++;
+        showHud();
+    }
+
+    public int getScreenWidth() {
+        return cam.getWidth();
+    }
+
+    public int getScreenHeight() {
+        return cam.getHeight();
+    }
+
+    @Override
+    public void onAction(String name, boolean isPressed, float tpf) {
+        if (!isPressed) {
+            return;
+        }
+
+        if (menuVisible) {
+            switch (name) {
+                case "MENU_UP":
+                    selectedMenuIndex--;
+                    if (selectedMenuIndex < 0) {
+                        selectedMenuIndex = 2;
+                    }
+                    showMenu();
+                    break;
+
+                case "MENU_DOWN":
+                    selectedMenuIndex++;
+                    if (selectedMenuIndex > 2) {
+                        selectedMenuIndex = 0;
+                    }
+                    showMenu();
+                    break;
+
+                case "SELECT":
+                    executeSelectedMenu();
+                    break;
+
+                case "CHANGE_PLAYERS":
+                    selectedMenuIndex = 1;
+                    changePlayers();
+                    break;
+
+                case "BACK_OR_QUIT":
+                    stop();
+                    break;
+
+                default:
+                    break;
+            }
+        } else {
+            switch (name) {
+                case "ADD_SCORE_LEFT":
+                    addScoreLeft();
+                    break;
+
+                case "ADD_SCORE_RIGHT":
+                    addScoreRight();
+                    break;
+
+                case "CHANGE_PLAYERS":
+                    changePlayers();
+                    break;
+
+                case "BACK_OR_QUIT":
+                    showMenu();
+                    break;
+
+                default:
+                    break;
+            }
+        }
+    }
 }
---
+++ src/main/java/ui/MainMenuUI.java
@@ -0,0 +1,82 @@
+package fr.univtln.pierre.samples.ui;
+
+import com.jme3.font.BitmapFont;
+import com.jme3.font.BitmapText;
+import com.jme3.math.ColorRGBA;
+import fr.univtln.pierre.samples.App;
+
+public class MainMenuUI {
+
+    private final App app;
+    private final BitmapFont font;
+
+    public MainMenuUI(App app) {
+        this.app = app;
+        this.font = app.getAssetManager().loadFont("Interface/Fonts/Default.fnt");
+    }
+
+    public void show(String player1, String player2, int selectedIndex) {
+        int width = app.getScreenWidth();
+        int height = app.getScreenHeight();
+
+        BitmapText title = new BitmapText(font);
+        title.setSize(font.getCharSet().getRenderedSize() * 2.8f);
+        title.setText("AIR HOCKEY");
+        title.setColor(ColorRGBA.Cyan);
+        title.setLocalTranslation(width / 2f - title.getLineWidth() / 2f, height - 80f, 0);
+        app.getGuiNode().attachChild(title);
+
+        BitmapText subtitle = new BitmapText(font);
+        subtitle.setSize(font.getCharSet().getRenderedSize() * 1.1f);
+        subtitle.setText("Menu principal");
+        subtitle.setColor(ColorRGBA.White);
+        subtitle.setLocalTranslation(width / 2f - subtitle.getLineWidth() / 2f, height - 130f, 0);
+        app.getGuiNode().attachChild(subtitle);
+
+        BitmapText intro = new BitmapText(font);
+        intro.setSize(font.getCharSet().getRenderedSize());
+        intro.setText("Utilise HAUT / BAS puis ENTREE");
+        intro.setColor(ColorRGBA.LightGray);
+        intro.setLocalTranslation(width / 2f - intro.getLineWidth() / 2f, height - 190f, 0);
+        app.getGuiNode().attachChild(intro);
+
+        BitmapText users = new BitmapText(font);
+        users.setSize(font.getCharSet().getRenderedSize());
+        users.setText("Utilisateurs : " + player1 + " VS " + player2);
+        users.setColor(ColorRGBA.Orange);
+        users.setLocalTranslation(width / 2f - users.getLineWidth() / 2f, height - 240f, 0);
+        app.getGuiNode().attachChild(users);
+
+        createMenuItem("[ JOUER ]", 0, selectedIndex, height - 340f);
+        createMenuItem("[ CHANGER UTILISATEURS ]", 1, selectedIndex, height - 400f);
+        createMenuItem("[ QUITTER ]", 2, selectedIndex, height - 460f);
+
+        BitmapText footer = new BitmapText(font);
+        footer.setSize(font.getCharSet().getRenderedSize() * 0.95f);
+        footer.setText("ECHAP = quitter | U = changer rapidement");
+        footer.setColor(ColorRGBA.Gray);
+        footer.setLocalTranslation(width / 2f - footer.getLineWidth() / 2f, 70f, 0);
+        app.getGuiNode().attachChild(footer);
+    }
+
+    private void createMenuItem(String text, int index, int selectedIndex, float y) {
+        BitmapText item = new BitmapText(font);
+        item.setSize(font.getCharSet().getRenderedSize() * 1.4f);
+
+        if (index == selectedIndex) {
+            item.setText("> " + text + " <");
+            item.setColor(ColorRGBA.Yellow);
+        } else {
+            item.setText(text);
+            item.setColor(ColorRGBA.White);
+        }
+
+        item.setLocalTranslation(
+                app.getScreenWidth() / 2f - item.getLineWidth() / 2f,
+                y,
+                0
+        );
+
+        app.getGuiNode().attachChild(item);
+    }
+}
